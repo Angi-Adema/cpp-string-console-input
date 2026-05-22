adbcf4831d5b76e4329eade6d7560a58518ba9f0
Angi-Adema <dr.adema@gmail.com>
2026-05-22 13:24:27 -0600

    removed several lines of commented code from top of file.

--- src/string-input.cpp
+++ src/string-input.cpp
@@ -1,10 +1,3 @@
-//============================================================================
-// Name        : string-input.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
 
 #include <iostream>
 #include <string>
