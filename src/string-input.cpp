6a740a7e0ca33b4e36c9b8d6e9074341012ce7b9
Angi-Adema <dr.adema@gmail.com>
2026-05-22 12:27:08 -0600

    added an array to hold final output text, prompted user for two animals as well as included reading in the response, then concatenated the response into meaningful output.

--- src/string-input.cpp
+++ src/string-input.cpp
@@ -7,9 +7,43 @@
 //============================================================================
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	// Array to hold final concatenated sentence
+	std::string dayAtTheZoo{};
+
+	// Prompt user to enter an animal they will visit at the zoo
+	std::cout << "Enter an animal you will visit at the zoo: ";
+
+	// Store user input
+	std::string animal1{};
+
+	// Read in user input
+	std::getline(std::cin >> std::ws, animal1);
+
+	// Prompt user to enter another animal
+	std::cout << "Enter another animal you will visit: ";
+
+	// Store user input
+	std::string animal2{};
+
+	// Read in user input
+	std::getline(std::cin >> std::ws, animal2);
+
+	// Add final content to dayAtTheZoo using concatenation
+	dayAtTheZoo = "Animals you will visit are " + animal1 + " and " + animal2 + ".";
+
+//	dayAtTheZoo = "Animals you will visit are ";
+//	dayAtTheZoo += animal1;
+//	dayAtTheZoo.append(" and ");
+//	dayAtTheZoo += animal2;
+//	dayAtTheZoo += ".";
+
+	// Output the content
+	std::cout << dayAtTheZoo;
+
 	return 0;
 }
